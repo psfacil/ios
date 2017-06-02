88c672516ad13c66d48972d3ec7deb143af67630
Usuário Convidado <Guest@L2602MICRO24.local>
2017-06-01 22:05:51 -0300

    fala cmg bb

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -10,9 +10,111 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var meuSegmento: UISegmentedControl!
+    
+    
+    @IBOutlet weak var lblAno: UILabel!
+    
+    
+    @IBOutlet weak var lblKM: UILabel!
+    
+    
+    @IBOutlet weak var lblRota: UILabel!
+    
+    
+    @IBOutlet weak var meuStepper: UIStepper!
+    
+    @IBAction func indexChanged(_ sender: Any) {
+        switch (meuSegmento.selectedSegmentIndex) {
+        case 0:
+            print("Voce escolheu o primeiro botao")
+            break
+        case 1:
+            print("Voce escolheu o segundo botao")
+            break
+        case 2:
+            print("Voce escolheu o \(meuSegmento.titleForSegment(at: meuSegmento.selectedSegmentIndex)!)")
+            break
+        default:
+            break
+        }
+    }
+    
+    
+    @IBAction func meuStepperChanged(_ sender: UIStepper) {
+        lblAno.text = "\(Int(sender.value))"
+    }
+    
+    
+    @IBAction func meuSliderChange(_ sender: UISlider) {
+        lblKM.text = "\(Int(sender.value))"
+    }
+    
+    
+    @IBAction func meuSwitch(_ sender: UISwitch) {
+        print(sender.isOn)
+    }
+    
+    
+    @IBAction func escolherRota(_ sender: Any) {
+        let alerta = UIAlertController(title:"Forma para receber a rota",
+                                       message: "Faca sua escolha",
+                                       preferredStyle:UIAlertControllerStyle.actionSheet                                                  )
+        
+        alerta.addAction(UIAlertAction(
+            title: "SMS",
+            style: UIAlertActionStyle.default,
+            handler:  {action in self.lblRota.text = action.title}
+            ))
+        
+        alerta.addAction(UIAlertAction(
+            title: "E-MAIL",
+            style: UIAlertActionStyle.default,
+            handler:  {action in self.lblRota.text = action.title}
+        ))
+        
+        alerta.addAction(UIAlertAction(
+            title: "Cancelar",
+            style: UIAlertActionStyle.default,
+            handler:  {action in self.lblRota.text = action.title}
+        ))
+        
+        present(alerta, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    @IBAction func salvarButao(_ sender: Any) {
+        var msg: String
+        var tipo: String
+        
+        if meuSegmento.selectedSegmentIndex >= 0{
+            tipo = meuSegmento.titleForSegment(at: meuSegmento.selectedSegmentIndex)!
+            msg = "Veiculo \(tipo) do ano \(lblAno.text!) salvo com sucesso"
+        }else{
+            tipo = ""
+            msg = "Antes de salvar escolha o tipo de veiculo"
+            
+        }
+        
+        let alerta = UIAlertController(
+            title: "Atencao",
+            message: msg,
+            preferredStyle: UIAlertControllerStyle.alert)
+        
+        alerta.addAction(UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil))
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lblAno.text = "1980"
+        lblKM.text = "7500"
     }
 
     override func didReceiveMemoryWarning() {
